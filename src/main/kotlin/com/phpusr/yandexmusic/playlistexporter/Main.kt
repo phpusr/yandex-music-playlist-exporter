731e5703d957e8e39deb48393256c21dbfa323ec
phpusr <phpusr@gmail.com>
2019-03-07 13:24:43 +0500

    add: gradle task for run

--- src/main/kotlin/com/phpusr/yandexmusic/playlistexporter/Main.kt
+++ src/main/kotlin/com/phpusr/yandexmusic/playlistexporter/Main.kt
@@ -2,6 +2,15 @@ package com.phpusr.yandexmusic.playlistexporter
 
 import com.phpusr.yandexmusic.playlistexporter.parser.JsonPlaylistParser
 
-fun main() {
-    JsonPlaylistParser.parse("phpusr", 1077)
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        System.err.println("> Wrong args number, found: ${args.size}, but must be: 2")
+        System.err.println(" - args: ${args.joinToString(separator = ", ")}")
+        return
+    }
+
+    val username = args[0]
+    val playlistId = args[1].toInt()
+
+    JsonPlaylistParser.parse(username, playlistId)
 }
